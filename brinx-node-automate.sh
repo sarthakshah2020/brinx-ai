dedb5ea3bfa15539e2800d7aef25ce128de49553
sarthakshah2020 <167911799+sarthakshah2020@users.noreply.github.com>
2024-09-23 16:49:48 +0530

    Create brinx-node-automate.sh

---
+++ brinx-node-automate.sh
@@ -0,0 +1,30 @@
+sudo apt-get install ufw
+
+sudo ufw allow 5011/tcp
+
+sudo ufw allow 22/tcp
+
+sudo ufw enable
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+docker pull admier/brinxai_nodes-worker:latest
+
+git clone https://github.com/admier1/BrinxAI-Worker-Nodes
+cd BrinxAI-Worker-Nodes
+chmod +x install_ubuntu.sh
+./install_ubuntu.sh
